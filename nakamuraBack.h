40267309316b0a03571aaf293efc211f89e7c3bb
janreggie <jan.delacruz@obf.ateneo.edu>
2018-04-07 12:40:03 +0800

    Added Runge-Kutta implementation
    Added Runge-Kutta implementation in writeToTemp(), which writes the
    values for the output voltage to a temporary file.
    Added C++11 option for cbp file.
    Added #include preprocessor in nakamuraApp.h to check whether
    nakamuraBack.h compiles.
    
    >gets destroyed by the ECCE dep't
    >gets destroyed by SA128 elective
    >gets destroyed by a thought of a love undeserved
    life is great.

--- nakamuraBack.h
+++ nakamuraBack.h
@@ -5,6 +5,8 @@ Backend for Nakamura, the RC Circuit Simulator
 
 #include <cstdio>
 #include <cmath>
+#include <string>
+
 #ifndef M_PI
   #define M_PI 3.14159265358979323846
 #endif
@@ -62,13 +64,49 @@ SYNTAX FOR TEMPORARY FILE: time-space-vout e.g.
 0.000002 1.99320
 */
 FILE * tpFile;
-tpFile = tmpfile();
 
 /*
 writeToTemp: void function that will do the appropriate maths (fucking ODEs)
 to the temporary file to be retrieved later.
 */
-void writeToTemp
+void writeToTemp ()
 {
-  // lol
+  // First, open up the file
+  tpFile = tmpfile();
+
+  // Determine our step size for time
+  // look at all the vars starting with TSt lol
+  double TStep = (TStop - TStart) / TNum;
+  // and now for lambda function
+  auto dVin = [&TStep](double t) -> double
+  {
+    return (Vin(t+TStep) - Vin(t)) / TStep;
+  };
+
+  // Note that we shall be using classical fourth-order Runge-Kutta
+  // Press et.al: Numerical Recipes The Art of Scientific Computing
+  // lambda function for the differential equation
+  auto f = [&dVin](double x, double y) -> double
+  {
+    return (dVin(x) - y/(Res*Cap));
+  };
+  // do some variables
+  double k1, k2, k3, k4;  // to be used for later; just allocate mem now
+  double TNow = 0;  // what's the current time?
+  double Vout = VNu;  // output voltage at a certain time (HIGHLY IMPORTANT)
+  while (TNow < TStop)
+  {
+    k1 = TStep * f(TNow, Vout);
+    k2 = TStep * f(TNow + TStep/2, Vout + k1/2);
+    k3 = TStep * f(TNow + TStep/2, Vout + k2/2);
+    k4 = TStep * f(TNow + TStep, Vout + k3);
+    Vout += k1/6 + k2/3 + k3/3 + k4/6;  // nothing should go wrong, rite?
+    TNow += TStep;
+    if (TNow > TStart)
+    {
+      // in that case, put it in the temp file
+      std::string temp = std::to_string(TNow)+' '+std::to_string(Vout)+'\n';
+      fputs(temp.c_str(), tpFile);
+    }
+  }
 }
